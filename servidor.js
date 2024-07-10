3cfbc5b0d3aee6ca83751d455e3f8290d82a61c5
juumachado <machado.julia@gsuite.iff.edu.br>
2024-07-10 10:37:23 -0300

    Atividade 10/07 Julia

--- servidor.js
+++ servidor.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-app.get('/', (request, response) => {
-    let n1 = 5 * 9
-    response.send(`O resutaltado é: ${n1}`)
+
+app.get('/conta', (request, response) => {
+    response.render ('conta')
 })
-app.listen(8080)
+const PORT = 8080
+app.listen(PORT, ()=> {})
+  console.log (`Servidor rodando em ${PORT}`);
