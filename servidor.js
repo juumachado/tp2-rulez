39e5196573050147ea6449fd018a6f978f860165
juumachado <machado.julia@gsuite.iff.edu.br>
2024-07-17 12:27:41 -0300

    Aula 17/07

--- servidor.js
+++ servidor.js
@@ -4,8 +4,33 @@ app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/conta', (request, response) => {
-    response.render ('conta')
+  response.render ('conta')
 })
+
+app.get('/tudonoback', (request, response) => {
+    response.render ('sol', {resultado: ''})
+})
+
+app.get('/soma', (request, response) => {
+  soma = parseFloat(request.query.n1) + parseFloat(request.query.n2)
+  response.render ('sol', {resultado: soma})
+})
+
+app.get('/sub', (request, response) => {
+  sub = parseFloat(request.query.n1) - parseFloat(request.query.n2)
+  response.render ('sol', {resultado: sub})
+})
+
+app.get('/mult', (request, response) => {
+  mult = parseFloat(request.query.n1) * parseFloat(request.query.n2)
+  response.render ('sol', {resultado: mult})
+})
+
+app.get('/div', (request, response) => {
+  div = parseFloat(request.query.n1) / parseFloat(request.query.n2)
+  response.render ('sol', {resultado: div})
+})
+
 const PORT = 8080
 app.listen(PORT, ()=> {})
   console.log (`Servidor rodando em ${PORT}`);
